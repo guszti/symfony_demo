39b13a142441d612c7bccdbb4b92ca018318a6f4
guszti <guszti@localhost.localdomain>
2019-07-27 14:51:40 +0200

    basic build

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,16 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PagesController extends AbstractController
 {
-    /**
-     * @Route("/", name="index")
-     */
     public function index()
     {
-        return $this->render('pages/index.html.twig', [
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'PagesController'
+        ]);
+    }
+
+    public function user($id){
+        return $this->render('user/index.html.twig', [
             'controller_name' => 'PagesController',
+            'id' => $id
+        ]);
+    }
+
+    public function editor(){
+        return $this->render('editor/index.html.twig', [
+            'controller_name' => 'PagesController'
+        ]);
+    }
+
+    public function administrator(){
+        return $this->render('administrator/index.html.twig', [
+            'controller_name' => 'PagesController'
         ]);
     }
 }
