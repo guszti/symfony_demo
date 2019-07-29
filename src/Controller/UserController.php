daa5fe2f338c03b1986b6332688e522d61fb567f
guszti <guszti@localhost.localdomain>
2019-07-29 12:54:44 +0200

    last login saved

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,15 +18,15 @@ class UserController extends AbstractController
      */
     public function createUsers(): Response
     {
-        // you can fetch the EntityManager via $this->getDoctrine()
+        /*// you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
-        /*$entityManager = $this->getDoctrine()->getManager();
+        $entityManager = $this->getDoctrine()->getManager();
 
         $newUser = new User();
         $newUser->setUsername('User3');
         $newUser->setRoles(['ROLE_USER']);
         $newUser->setPassword('user');
-        $newUser->setLastonline();
+        $newUser->setLastonline(new \DateTime("now"));
 
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($newUser);
@@ -34,8 +34,8 @@ class UserController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
-        return new Response('Saved new product with id '.$newUser->getId());*/
-
+        return new Response('Saved new product with id '.$newUser->getId());
+        */
         return $this->redirectToRoute('dashboard');
     }
 }
